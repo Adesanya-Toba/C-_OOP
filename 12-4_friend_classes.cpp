73fb93fbf75932257a4cdfde82e57444b6e1d394
Toba <adesanyatoba@gmail.com>
2021-10-06 04:58:32 +0100

    12-4_friend_classes

---
+++ 12-4_friend_classes.cpp
@@ -0,0 +1,48 @@
+/**
+ * C++: The Complete Reference
+ * by Herbert Schidt
+ * 
+ * Friend class
+ * 
+ * The friend class and all of its member functions
+ * have access to the private members defined within the other class
+*/
+#include <iostream>
+using namespace std;
+
+class TwoValues
+{
+    int a , b;
+
+public:
+    TwoValues (int i, int j){
+        a = i;
+        b = j;
+    }
+    friend class Min;
+};
+
+/**
+ * @brief   Friend classes do not inherit members of the other class,
+ *          they only have access to it.
+*/
+class Min
+{
+public:
+    int min(TwoValues x);
+};
+
+int Min::min (TwoValues x)
+{
+    return x.a < x.b ? x.a : x.b;
+}
+
+int main()
+{
+    TwoValues ob (10, 20);
+    Min m;
+
+    cout << m.min(ob);
+
+    return 0;
+}
