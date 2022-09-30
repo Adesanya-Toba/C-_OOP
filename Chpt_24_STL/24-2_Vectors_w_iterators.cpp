65eea55d0419360a0cb653f48ca1bb95364a8baa
Toba-Adesanya <adesanyatoba@gmail.com>
2022-09-30 09:55:15 +0100

    24-2 sort vectors with iterators

--- Chpt_24_STL/24-2_Vectors_w_iterators.cpp
+++ Chpt_24_STL/24-2_Vectors_w_iterators.cpp
@@ -12,8 +12,32 @@
 #include <iostream>
 #include <vector>
 #include <cctype>
+#include <bits/stdc++.h>
 
 using namespace std;
+
+bool customCompare(vector<int> &a, vector<int> &b)
+{
+    vector<int>::iterator p1, p2;
+    p1 = a.begin();
+    p2 = b.begin();
+    int diffA = a[0] - a[1];
+    int diffB = b[0] - b[1];
+
+    // p1++; p2++;
+    // return diffA < diffB;
+    return (*p1 - *(p1 + 1)) < *p2 - *(p2 +1);
+}
+
+bool customSort(vector<int> &a, vector<int> &b)
+{
+    // Sort the individual vectors first
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    
+    // Sort according to the first element of each vector
+    return *a.begin() < *b.begin();
+}
 
 int main()
 {
@@ -59,6 +83,23 @@ int main()
     while (p != v.end()){
         cout << *p << " ";
         p++;
+    }
+    cout << endl;
+
+    vector<int> a {9,0,4,5,2,5,7};
+    sort(a.begin(), a.end(), greater<int>());
+
+    for (const auto& v: a) cout << v << " ";
+    cout << "\n" << endl;
+
+    vector<vector<int>> b {{2,3}, {4,7}, {9,0}, {6,1}};
+    sort(b.begin(), b.end(), customSort);
+
+    vector<vector<int>>::iterator p2 = b.begin();
+
+    for (const auto& v: b) {
+        for(auto& v2 : v)
+        cout << v2 << " ";
     }
     cout << endl;
 
